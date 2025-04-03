1780309cce8fc4c94491e66f75c10800f1c032ae
Caio Victor <caio.araujo@ccc.ufcg.edu.br>
2025-04-03 19:43:45 -0300

    revisao final

--- main.py
+++ main.py
@@ -15,19 +15,16 @@ def process_drive_upload():
             create_and_upload_new_file(drive, max_file)
         cont += 1
 
-## Roda a block chain
 def run_blockchain():
-    
-    # Criando múltiplas threads para execução simultânea
+
     threads = []
-    num_threads = 10  # Número de threads para rodar em paralelo
+    num_threads = 10
 
     for _ in range(num_threads):
         thread = threading.Thread(target=process_drive_upload)
         threads.append(thread)
         thread.start()
 
-    # Aguarda todas as threads finalizarem
     for thread in threads:
         thread.join()
 
@@ -35,11 +32,9 @@ def run_blockchain():
 
     print("Precisamos encontrar a maior cadeia!!!!")
 
-## Roda o algoritmo de identificação da chan principal
 def search_channels():
     drive = get_drive_service()
     ch = Channels(drive)
-    ## Use para gerar o arquivo de texto chan01, que é o arquivo que contem a chan principal
     ch.make()
 
 run_blockchain()
